53884e7bff5299c5269d834d3cf4dfdae0a352cc
Andrew Anderson <andy@clubanderson.com>
2024-04-07 16:32:04 -0400

    updated sha and url for  brew installer for labeler

--- brew/Formula/labeler.rb
+++ brew/Formula/labeler.rb
@@ -1,8 +1,8 @@
 class Labeler < Formula
     desc "Utility that automates the labeling of resources output from kubectl, kustomize, and helm"
-    homepage "https://github.com/clubanderson/labeler"
-    url "https://github.com/clubanderson/labeler/releases/download/v0.1.0/labeler-v0.1.0.tar.gz"
-    sha256 "0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef"
+    homepage "https://github.com/clubanderson/hackathon-labeler"
+    url "https://github.com/clubanderson/hackathon-labeler/releases/download/v0.1.0/labeler"
+    sha256 "be174998d8a312930897bae18342b6721f060aba6748b527506c95e3011fa535"
   
     def install
       bin.install "labeler"
