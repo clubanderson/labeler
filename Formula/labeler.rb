1e61684ae07c113b1848dffed0de382452b2cbbb
Andrew Anderson <andy@clubanderson.com>
2024-04-08 14:44:49 -0400

    added --version, -v support and added output to brew installer

--- Formula/labeler.rb
+++ Formula/labeler.rb
@@ -1,7 +1,7 @@
 class Labeler < Formula
   desc "Utility that automates the labeling of resources output from kubectl, kustomize, and helm"
-  homepage "https://github.com/clubanderson/hackathonlabeler"
-  url "https://github.com/clubanderson/hackathonlabeler/releases/download/v0.2.0/labeler"
+  homepage "https://github.com/clubanderson/labeler"
+  url "https://github.com/clubanderson/labeler/releases/download/v0.2.0/labeler"
   # sha256 "be174998d8a312930897bae18342b6721f060aba6748b527506c95e3011fa535"
 
   def install
