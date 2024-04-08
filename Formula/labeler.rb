e832d67a11a003a41e8e37f023b403a7d1a94d7a
Andrew Anderson <andy@clubanderson.com>
2024-04-08 14:50:31 -0400

    added --version, -v support and added output to brew installer

--- Formula/labeler.rb
+++ Formula/labeler.rb
@@ -19,29 +19,29 @@ class Labeler < Formula
       
       For Bash users:
          \e[33mecho 'alias kl="labeler kubectl"'\e[0m >> ~/.bashrc
-         \e[33echo 'alias hl="labeler helm"'\e[0m >> ~/.bashrc
-         \e[33source ~/.bashrc\e[0m
+         \e[33mecho 'alias hl="labeler helm"'\e[0m >> ~/.bashrc
+         \e[33msource ~/.bashrc\e[0m
       
       For Zsh users:
-         \e[33echo 'alias kl="labeler kubectl"'\e[0m >> ~/.zshrc
-         \e[33echo 'alias hl="labeler helm"'\e[0m >> ~/.zshrc
-         \e[33source ~/.zshrc\e[0m
+         \e[33mecho 'alias kl="labeler kubectl"'\e[0m >> ~/.zshrc
+         \e[33mecho 'alias hl="labeler helm"'\e[0m >> ~/.zshrc
+         \e[33msource ~/.zshrc\e[0m
 
       Then just use `kl` or `hl` in place of `kubectl` or `helm` respectively. Add -l or --label to the end of the command to label ALL of the resources you apply.
 
       example (kubectl):
         
-         kl apply -f examples/kubectl/pass -l app.kubernetes.io/part-of=sample --context=kind-kind --namespace=default --overwrite
+         kl apply -f examples/kubectl/pass \e[33m-l app.kubernetes.io/part-of=sample\e[0m --context=kind-kind --namespace=default --overwrite
 
       example (kustomize):
 
-         kl apply -k examples/kustomize -l app.kubernetes.io/part-of=sample-app --context=kind-kind --namespace=temp --overwrite
+         kl apply -k examples/kustomize \e[33m-l app.kubernetes.io/part-of=sample-app\e[0m --context=kind-kind --namespace=temp --overwrite
 
       example (helm):
 
-         hl --kube-context=kind-kind template sealed-secrets sealed-secrets/sealed-secrets -n sealed-secrets --create-namespace --label=app.kubernetes.io/part-of=sample-app --dry-run   
+         hl --kube-context=kind-kind template sealed-secrets sealed-secrets/sealed-secrets -n sealed-secrets --create-namespace \e[33m--label=app.kubernetes.io/part-of=sample-app\e[0m --dry-run   
       
-      For more information, visit the readme at https://github.com/clubanderson/labeler
+      For more information, visit the readme at \e[33mhttps://github.com/clubanderson/labeler\e[0m
     EOS
   end
 end
