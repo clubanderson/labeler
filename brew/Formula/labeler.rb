9d5ceac99def55f27c1778b5b10e9a2d51f96687
Andrew Anderson <andy@clubanderson.com>
2024-04-08 14:57:05 -0400

    updates to brew installer

--- brew/Formula/labeler.rb
+++ brew/Formula/labeler.rb
@@ -1,8 +1,8 @@
 class Labeler < Formula
   desc "Utility that automates the labeling of resources output from kubectl, kustomize, and helm"
   homepage "https://github.com/clubanderson/labeler"
-  url "https://github.com/clubanderson/labeler/releases/download/v0.2.0/labeler"
-  sha256 "ea8a03f413c0f22b172ed9257156ea77195bf7ebaefc3acd3775fcba9343fa35"
+  url "https://github.com/clubanderson/labeler/releases/download/v0.3.0/labeler"
+  # sha256 "ea8a03f413c0f22b172ed9257156ea77195bf7ebaefc3acd3775fcba9343fa35"
 
   def install
     bin.install "labeler"
@@ -16,32 +16,26 @@ class Labeler < Formula
   def caveats
     <<~EOS
       To make using labeler more convenient, consider creating aliases:
-      
-      For Bash users:
-         \e[33mecho 'alias kl="labeler kubectl"'\e[0m >> ~/.bashrc
-         \e[33echo 'alias hl="labeler helm"'\e[0m >> ~/.bashrc
-         \e[33source ~/.bashrc\e[0m
-      
-      For Zsh users:
-         \e[33echo 'alias kl="labeler kubectl"'\e[0m >> ~/.zshrc
-         \e[33echo 'alias hl="labeler helm"'\e[0m >> ~/.zshrc
-         \e[33source ~/.zshrc\e[0m
+      \e[33malias kl="labeler kubectl"\e[0m
+      \e[33malias hl="labeler helm"\e[0m
 
-      Then just use `kl` or `hl` in place of `kubectl` or `helm` respectively. Add -l or --label to the end of the command to label ALL of the resources you apply.
+      (if you want these to be permanent, add these to your shell profile, e.g. ~/.bashrc or ~/.zshrc, then source it)
+
+      Then just use `kl` or `hl` in place of `kubectl` or `helm` respectively. Add -l or --label= to the end of the command to label ALL of the resources you apply.
 
       example (kubectl):
         
-         kl apply -f examples/kubectl/pass -l app.kubernetes.io/part-of=sample --context=kind-kind --namespace=default --overwrite
+         kl apply -f examples/kubectl/pass \e[33m-l app.kubernetes.io/part-of=sample\e[0m --context=kind-kind --namespace=default --overwrite
 
       example (kustomize):
 
-         kl apply -k examples/kustomize -l app.kubernetes.io/part-of=sample-app --context=kind-kind --namespace=temp --overwrite
+         kl apply -k examples/kustomize \e[33m-l app.kubernetes.io/part-of=sample-app\e[0m --context=kind-kind --namespace=temp --overwrite
 
       example (helm):
 
-         hl --kube-context=kind-kind template sealed-secrets sealed-secrets/sealed-secrets -n sealed-secrets --create-namespace --label=app.kubernetes.io/part-of=sample-app --dry-run   
+         hl --kube-context=kind-kind template sealed-secrets sealed-secrets/sealed-secrets -n sealed-secrets --create-namespace \e[33m--label=app.kubernetes.io/part-of=sample-app\e[0m --dry-run   
       
-      For more information, visit the readme at https://github.com/clubanderson/labeler
+      For more information, or to make labeler better, visit the readme at \e[33mhttps://github.com/clubanderson/labeler\e[0m
     EOS
   end
 end
